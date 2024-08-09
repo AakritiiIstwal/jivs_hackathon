81aa141d6dc48a6b7cc176e161ae62f73329c089
Aakriti Istwal <istwalaakriti@gmail.com>
2024-08-09 12:38:49 +0200

    Added rag script

--- rag.py
+++ rag.py
@@ -9,50 +9,42 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.text import TextLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
-
-
-
-os.environ["OPENAI_API_KEY"] = getpass.getpass()
-
+from huggingface_hub import InferenceClient
+from langchain.llms import HuggingFaceEndpoint
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-#CSV AGENT-------------
-
-# # Create the CSV agent
-# agent = create_csv_agent(llm, "sap_dummy_data_dd02t.csv", verbose=True, allow_dangerous_code=True)
-# def query_data(query):
-#     response = agent.invoke(query)
-#     return response
-
-# query = "Can you tell me something about tabname MARA?"
-# response = query_data(query)
-# print(response)
-
-#CSV AGENT-------------
-
-
-#TEXT LOADER------------
-
-# # Load, chunk and index the contents of the blog.
-# loader = WebBaseLoader(
-#     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-#     bs_kwargs=dict(
-#         parse_only=bs4.SoupStrainer(
-#             class_=("post-content", "post-title", "post-header")
-#         )
-#     ),
-# )
-# docs = loader.load()
-#TEXT LOADER------------
-
+# llm = HuggingFaceEndpoint(
+#             endpoint_url="https://q2sxm30ut74mj3w3.us-east-1.aws.endpoints.huggingface.cloud/",
+#             huggingfacehub_api_token="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",
+#             task="text-generation",
+#             max_new_tokens=150, 
+#             do_sample=True, 
+#             top_p=0.9, 
+#             temperature=0.2
+#             )
 loader = TextLoader("target.txt")
 docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
+
+
+# #remove
+# # Hugging-Face Embeddings
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2",
+#     model_kwargs={"device": "cpu"},
+# )
+# vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+
+
 vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
 # Retrieve and generate using the relevant snippets of the blog.
@@ -72,8 +64,8 @@ rag_chain = (
 )
 
 query = '''Find if the table is relevant to the target database for the following description: 
-"This table contains customer information such as names, contact details, and demographic data. The key columns include a unique identifier for each customer, the customer's first name, last name, email, phone number, date of birth, address, city, state, zip code, and registration date."
-If yes, then give the source and target column mappings'''
+"This table stores information about projects, including their planned start and finish dates, planning method, budget authorization key, and project definition. The table provides a comprehensive overview of project planning and execution, enabling effective monitoring and control of project progress."
+If yes, then give the source and target column mappings.'''
 
 for chunk in rag_chain.stream(query):
     print(chunk, end="", flush=True)
